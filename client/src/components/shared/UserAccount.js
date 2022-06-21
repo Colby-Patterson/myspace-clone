9e8f27d0b0f68a4973ef23aff43de5ee06b0c6a4
Colby Patterson <cpatt@byui.edu>
2022-06-21 17:15:38 -0600

    Added to UserAccount

--- client/src/components/shared/UserAccount.js
+++ client/src/components/shared/UserAccount.js
@@ -1,8 +1,27 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../providers/AuthProvider";
 
 const UserAccount = () => {
   const {user} = useContext(AuthContext)
+  const navigate = useNavigate()
+  const [fakeUsers, setFakeUsers] = useState([])
+
+  useEffect(()=>{
+    getFakeUsers()
+  }, [])
+
+  const getFakeUsers = async () => {
+    try {
+      let res = await axios.get('/api/fake_users')
+      setFakeUsers(res.data)
+    } catch(err) {
+      alert('Error occurred with getFakeUsers')
+      console.log(err)
+    }
+  }
+
   return (
     <div>
       <h1>Account Settings</h1>
